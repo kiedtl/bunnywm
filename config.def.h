eee1ef5e3b75fc7fcbf0eddbf93622850d1513d2
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-12-18 08:47:05 -0500

    config.def.h: update

--- config.def.h
+++ config.def.h
@@ -1,34 +1,56 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// ----- definitions -----
+// only mess with this if you know
+// what you're doing!
 #define MOD Mod4Mask
 
-const char* menu[]    = {"dmenu_run",      0};
-const char* term[]    = {"st",             0};
-const char* scrot[]   = {"scr",            0};
-const char* briup[]   = {"bri", "10", "+", 0};
-const char* bridown[] = {"bri", "10", "-", 0};
-const char* voldown[] = {"amixer", "sset", "Master", "5%-",         0};
-const char* volup[]   = {"amixer", "sset", "Master", "5%+",         0};
-const char* volmute[] = {"amixer", "sset", "Master", "toggle",      0};
-const char* colors[]  = {"bud", "/home/goldie/Pictures/Wallpapers", 0};
+#define RESIZE_LEFT		0x1
+#define RESIZE_DOWN		0x2
+#define RESIZE_UP		0x3
+#define RESIZE_RIGHT	0x4
+#define MOVE_LEFT		0x5
+#define MOVE_DOWN		0x6
+#define MOVE_UP			0x7
+#define MOVE_RIGHT		0x8
 
+// ----- command -----
+const char *lock[]    = { "slock",                       0 };
+const char *menu[]    = { "ndmen",                       0 };
+const char *term[]    = { "xterm", "-title", "terminal", 0 };
+const char *scrot[]   = { "scr",                         0 };
+
+// ------ keybindings ------
 static struct key keys[] = {
-    {MOD,      XK_q,   win_kill,   {0}},
-    {MOD,      XK_c,   win_center, {0}},
-    {MOD,      XK_f,   win_fs,     {0}},
-    {Mod1Mask, XK_Tab, win_next,   {0}},
+    {MOD|ShiftMask, XK_e,   sowm_exit,  {0}},
+    {MOD|ShiftMask, XK_q,   win_kill,   {0}},
+    {MOD,           XK_c,   win_center, {0}},
+    {MOD,           XK_f,   win_fs,     {0}},
+    {MOD,           XK_j,   win_next,   {0}},
+    {MOD,           XK_k,   win_prev,   {0}},
+    //{Mod1Mask,     XK_Tab, win_next,   {0}},
+	
+	{Mod1Mask,      XK_h,   win_modify, {.i = RESIZE_LEFT}},
+	{Mod1Mask,      XK_j,   win_modify, {.i = RESIZE_DOWN}},
+	{Mod1Mask,      XK_k,   win_modify, {.i = RESIZE_UP}},
+	{Mod1Mask,      XK_l,   win_modify, {.i = RESIZE_RIGHT}},
 
-    {MOD, XK_d,      run, {.com = menu}},
-    {MOD, XK_w,      run, {.com = colors}},
-    {MOD, XK_p,      run, {.com = scrot}},
-    {MOD, XK_Return, run, {.com = term}},
+	{MOD|ShiftMask, XK_h,   win_modify, {.i = MOVE_LEFT}},
+	{MOD|ShiftMask, XK_j,   win_modify, {.i = MOVE_DOWN}},
+	{MOD|ShiftMask, XK_k,   win_modify, {.i = MOVE_UP}},
+	{MOD|ShiftMask, XK_l,   win_modify, {.i = MOVE_RIGHT}},
 
-    {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
-    {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
-    {0,   XF86XK_AudioMute,         run, {.com = volmute}},
-    {0,   XF86XK_MonBrightnessUp,   run, {.com = briup}},
-    {0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},
+	{MOD|ShiftMask, XK_S,      run, {.com = lock}},
+    {MOD,           XK_p,      run, {.com = menu}},
+    {MOD|ShiftMask, XK_p,      run, {.com = scrot}},
+    {MOD,           XK_Return, run, {.com = term}},
+
+    //{0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
+    //{0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
+    //{0,   XF86XK_AudioMute,         run, {.com = volmute}},
+    //{0,   XF86XK_MonBrightnessUp,   run, {.com = briup}},
+    //{0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},
 
     {MOD,           XK_1, ws_go,     {.i = 1}},
     {MOD|ShiftMask, XK_1, win_to_ws, {.i = 1}},
